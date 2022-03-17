6b38c58f74e1abe5ba3306534f110d6362a8d0be
Athlon007 <aathlon@outlook.com>
2022-03-17 21:09:51 +0100

    Update copy.bat

--- Batch/copy.bat
+++ Batch/copy.bat
@@ -1,16 +1,16 @@
 @echo off
 
-set MSCLoaderPath=D:\SteamLibrary\steamapps\common\My Summer Car
-set MLPPath=D:\SteamLibrary\steamapps\common\My Summer Car ProLoader
+set MSCLoaderPath=C:\Program Files (x86)\Steam\steamapps\common\My Summer Car
+set MLPPath=C:\Program Files (x86)\Steam\steamapps\common\My Summer Car ProLoader
 set ToPath=%MSCLoaderPath%\Mods
 set ToMLP=%MLPPath%\Mods
 echo.
 
-CALL :copy_file "C:\Users\aathl\source\repos\MOP\MOP\bin\Debug\MOP.dll"
-CALL :copy_file "C:\Users\aathl\source\repos\Fifth Gear\bin\Debug\FifthGear.dll"
-CALL :copy_file "C:\Users\aathl\source\repos\SaveSlots\SaveSlots\bin\Debug\SaveSlots.dll"
+CALL :copy_file_to "\\Mac\Home\Developer\MOP\MOP\bin\Debug\MOP.dll" "%ToPath%"
+CALL :copy_file_to "\\Mac\Home\Developer\SaveSlots\SaveSlots\bin\Debug\SaveSlots.dll" "%ToPath%"
 
-CALL :copy_file_to "C:\Users\aathl\source\repos\MOP\MOP\bin\ProRelease\MOP.dll" "%ToMLP%"
+CALL :copy_file_to "\\Mac\Home\Developer\MOP\MOP\bin\ProRelease\MOP.dll" "%ToMLP%"
+CALL :copy_file_to "\\Mac\Home\Developer\SaveSlots\SaveSlots\bin\ProRelease\SaveSlots.dll" "%ToMLP%"
 
 echo.
 echo Done!
@@ -21,22 +21,11 @@ ECHO 3 - EXIT
 ECHO.
 SET /P M=Type 1, 2, or 3 then press ENTER: 
 
-IF %M%==1 start "" /D "%MSCLoaderPath%\mysummercar.exe"
-IF %M%==2 start "" /D "%MLPPath%\mysummercar.exe"
+IF %M%==1 start "" "%MSCLoaderPath%\mysummercar.exe"
+IF %M%==2 start "" "%MLPPath%\mysummercar.exe"
 IF %M%==3 GOTO EOF
 
 EXIT
-
-
-
-:copy_file
-    if exist %~1 (
-        echo "%~1 => %ToPath%"
-        xcopy /Y /Q "%~1" "%ToPath%" > Nul
-    ) else (
-        echo File %~1 does not exist!
-    )
-    EXIT /B 0
 
 :copy_file_to
     if exist %~1 (
