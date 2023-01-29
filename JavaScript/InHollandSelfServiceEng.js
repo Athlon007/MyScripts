fe9049b0ec37f7d6eb8b375b2199821fd1cd4a4b
Athlon007 <aathlon@outlook.com>
2023-01-29 13:52:10 +0100

    Update InHollandSelfServiceEng.js

--- JavaScript/InHollandSelfServiceEng.js
+++ JavaScript/InHollandSelfServiceEng.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Selfservice Switch Language ToEnglish
+// @name         Selfservice Switch Language To English
 // @namespace    Athlon/InHollandSelfservice
 // @version      1.0
 // @description  Automatically switch language to English in Selfservice
@@ -8,10 +8,12 @@
 // @run-at       document-end
 // ==/UserScript==
 
-const btnSwitchLanguage = document.getElementById('IH_FLUID_WRK_LANGUAGECD$20$');
-const imgLang = btnSwitchLanguage.children[1].src;
-const srcLangEng = "/cs/CS92PRD/cache/IH_UK_FLAG_24_DUT_1.PNG";
+window.addEventListener('load', function () {
+    const btnSwitchLanguage = document.getElementById('IH_FLUID_WRK_LANGUAGECD$20$');
+    const imgLang = btnSwitchLanguage.children[1].src;
+    const srcLangEng = "/cs/CS92PRD/cache/IH_UK_FLAG_24_DUT_1.PNG";
 
-if (imgLang === srcLangEng) {
-    btnSwitchLanguage.click();
+    if (imgLang === srcLangEng) {
+        btnSwitchLanguage.click();
+    }
 }
